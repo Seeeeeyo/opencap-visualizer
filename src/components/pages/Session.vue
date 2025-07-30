54856e9ee6fcbbfd3317708f52abffd7d8837a93
Seeeeeyo <selimgilon@gmail.com>
2025-07-30 15:42:58 +0200

    no fps set

--- src/components/pages/Session.vue
+++ src/components/pages/Session.vue
@@ -6626,7 +6626,7 @@ const axiosInstance = axios.create();
 
     // Start recording with timeslices to ensure data is captured in smaller chunks
     // This helps with memory usage and ensures more consistent recording
-    this.mediaRecorder.start(40); // Capture in 1-second intervals
+    this.mediaRecorder.start(); 
       this.isRecording = true;
 
       // If not already playing, start playback
