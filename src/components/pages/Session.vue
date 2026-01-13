698bfdf05d88d663fc70bd538ecd70119a13cdfb
Seeeeeyo <selimgilon@gmail.com>
2026-01-13 13:54:16 -0700

    github btn

--- src/components/pages/Session.vue
+++ src/components/pages/Session.vue
@@ -2058,16 +2058,15 @@
         <template v-slot:activator="{ on, attrs }">
           <v-btn
             icon
-            dark
-            small
             class="github-link-btn"
-            href="https://github.com/Seeeeeyo/opencap-visualizer"
-            target="_blank"
+            @click.stop="openGitHubRepo"
             v-bind="attrs"
             v-on="on"
             v-if="$route.query.embed !== 'true'"
           >
-            <v-icon small>mdi-github</v-icon>
+            <svg class="github-icon" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+              <path d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"/>
+            </svg>
           </v-btn>
         </template>
         <span>View on GitHub</span>
@@ -4330,6 +4329,9 @@
       }
     },
     methods: {
+    openGitHubRepo() {
+      window.open('https://github.com/Seeeeeyo/opencap-visualizer', '_blank');
+    },
 
     // Animation details toggle methods
     toggleAnimationDetails(index) {
@@ -16407,6 +16409,23 @@
     background: rgba(40, 40, 40, 0.8) !important;
     border-radius: 50%;
     transition: all 0.2s ease;
+    width: 36px !important;
+    height: 36px !important;
+    min-width: 36px !important;
+    display: flex !important;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .github-link-btn .github-icon {
+    width: 20px;
+    height: 20px;
+    fill: rgba(255, 255, 255, 0.9) !important;
+    display: block;
+  }
+
+  .github-link-btn:hover .github-icon {
+    fill: white !important;
   }
 
   .github-link-btn:hover {
